74c313a5ce717826ae9a1507d4e72931d094b87f
1939123 <114641673+1939123@users.noreply.github.com>
2023-11-10 07:05:13 -0600

    Add files via upload

---
+++ lab-07/src/main/java/sa/edu/kau/fcit/cpit252/ImageElementsFactory.java
@@ -0,0 +1,25 @@
+package sa.edu.kau.fcit.cpit252;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class ImageElementsFactory {
+
+  private Map<String, ImageElement> flyweights = new HashMap<String, ImageElement>();
+
+  public ImageElement getFlyweight(String name) {
+    // return a flyweight if it already exists, otherwise, put it in the map.
+    if(flyweights.containsKey(name)){
+      return flyweights.get(name);
+    }
+      ImageElement imageElement = new ImageElement(name);
+  flyweights.put(name, imageElement);
+  return imageElement;
+  }
+
+  public int numberOfFlyweights() {
+    // return the size of the HashMap
+    return flyweights.size();
+  }
+
+}
